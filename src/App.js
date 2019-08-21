10729399c0c2d52870a083cb8aa58bc0f009a88f
Greg Smith <greg.smith0@walmart.com>
2019-08-21 13:34:48 -0700

    learning react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+class RegularClass {}
+class ComponentClass extends Component {}
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log('regularClassInstance', regularClassInstance);
+console.log('componentClassInstance', componentClassInstance);
+*/
+
+class App extends Component {
+
+   constructor(){
+      super();
+      this.state = { displayBio: false };
+
+      // allow the helper method to access the 'this' of the Component
+      this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+   }
+
+   // setState is a helper method in the Component class
+   // its input is an object
+   toggleDisplayBio() {
+      this.setState({ displayBio: !this.state.displayBio });
+   }
+
+   render(){
+
+      // option, remove this as a const and just put the whole thing
+      // in place of the bio variable, below, in the JSX
+      /*
+      const bio = this.state.displayBio ? (
+         <div>
+            <p>I am a robot.</p>
+            <p>I will race you.</p>
+            <p>My skin is very shiny.</p>
+         </div>
+      ) : null;
+      */
+
+      // this can be replaced with a ternary expression, shown above
+      // and then change bio from let to const, since it's never reassigned?
+      /*
+      if (!this.state.displayBio){
+         bio = null;
+      }
+      */
+
+      return (
+         <div>
+            <h1>Hello</h1>
+            <p>All your base are belong to me.</p>
+            {
+               this.state.displayBio ? (
+                  <div>
+                     <p>I am a robot.</p>
+                     <p>I will race you.</p>
+                     <p>My skin is very shiny.</p>
+                     <button onClick={this.toggleDisplayBio}>Show Less</button>
+                  </div>
+               ) : (
+                  <button onClick={this.toggleDisplayBio}>Read More</button>
+               )
+            }
+         </div>
+      )
+   }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+// shortened below: ReactDOM.render(<App></App>, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+class Animal {
+   constructor(name, age) {
+      this.name = name;
+      this.age = age;
+   }
+
+   // add another custom method
+   speak(){
+      console.log('I am', this.name, 'and I am', this.age, 'years old.');
+   }
+}
+
+// create instances of the Animal class (these class instances are objects)
+// ------------------------ invoke the constructor of the Animal class
+const animal1 = new Animal('Simba', 3);
+// call its speak method
+animal1.speak();
+
+// log the animal1 object
+console.log(animal1);
+
+class Lion extends Animal {
+   constructor(name, age, furColor, speed) {
+      // 'super' keyword invokes the constructor of the parent class
+      super(name, age);
+      this.furColor = furColor;
+      this.speed = speed;
+   }
+
+   // new custom method
+   roar() {
+      console.log(
+         'ROAR! I have', 
+         this.furColor, 
+         'fur, and I can run', 
+         this.speed, 
+         'miles per hour!'
+         );
+   }
+}
+
+// make an instance of the Lion class
+const lion1 = new Lion('Mufasa', 20, 'golden', 25);
+// we have access to the speak method, since it's extended from Animal
+lion1.speak();
+// call the new roar method
+lion1.roar();
+console.log(lion1);
+*/
